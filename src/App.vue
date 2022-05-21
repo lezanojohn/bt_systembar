9b6d4ca506c368bc1e0e60ddbf4d04f74c7e4701
John Lezano <lezanojohn@gmail.com>
2022-05-21 18:07:36 -0400

    Agregando Vuetify y Wireframe System Bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Blog de telecontrol</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <template v-for="category in categories">
+            <v-col
+              :key="category.id"
+              class="mt-2"
+              cols="12"
+            >
+              <strong>{{ category.name }}</strong>
+            </v-col>
+
+            <v-col
+              v-for="j in 6"
+              :key="`${category.id}${j}`"
+              cols="6"
+              md="2"
+            >
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      categories: [
+        { id: 0, name: 'Agua Potable' },
+        { id: 1, name: 'Agua Servida' },
+        { id: 2, name: 'Distribución' },
+        { id: 3, name: 'Recolección' },
+        { id: 4, name: 'Mesa Soporte' },
+        { id: 8, name: 'Fiscalizaciones' },
+        { id: 6, name: 'Analítica' },
+        { id: 5, name: 'Informes' },
+        { id: 7, name: 'Extracción de datos' },
+      ]
+    }),
   }
-}
-</style>
+</script>
